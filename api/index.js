77a619a6e51a15d55b184ab4c5b09a4d1b3e19f1
Muazhere5 <muazctg07@gmail.com>
2025-11-20 18:30:46 +0600

    ExactProductionDomain

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use(cors({
     'http://localhost:5174',
     'http://localhost:5175',
     'https://your-client-site.netlify.app',
-    'https://serverside-l2nb51302-muaz-heres-projects.vercel.app/'
+    'https://serverside-xi.vercel.app/'
   ],
   credentials: true
 }));
